5bc032bd97acc42123f9f61b388d1a21c0beded5
pratyakshi <ratya.g@protonmail.com>
2024-02-04 16:14:07 +0530

    Made the appBar + tasks button

---
+++ android/app/src/main/kotlin/com/example/swifttasks/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.swifttasks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'SwiftTasks',
+    theme: ThemeData(), // customize theme
+    home: const HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 243, 242, 241),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 213, 215, 255),
+        foregroundColor: Color.fromARGB(255, 61, 61, 61),
+        elevation: 0,
+        title: const ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Color.fromARGB(255, 147, 136, 180),
+            radius: 30,
+            // child: Image.asset('assets/profile.jpg'), remove const
+          ),
+          title: Text('Rat'),
+          subtitle: Text(
+            'Tasks To Do',
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.calendar_month),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.notifications),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Gap(20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Today\'s Tasks',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 201, 181, 255),
+                      foregroundColor: Color.fromARGB(255, 10, 11, 104),
+                      elevation: 0,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      )),
+                  onPressed: () {},
+                  child: Text(
+                    '+ New Task',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Text('Saturday, 3 February'),
+          ],
+        ),
+      ),
+    );
+  }
+}
